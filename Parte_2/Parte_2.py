2a17000eec9cb5e5982a3f6cc2e80f0ccffc378a
minimum16 <74023952+minimum16@users.noreply.github.com>
2020-12-26 17:35:07 +0000

    Changes

--- Parte_2/Parte_2.py
+++ Parte_2/Parte_2.py
@@ -5,24 +5,21 @@ from mewpy.simulation import get_simulator
 #b)
 model = cobra.io.sbml.read_sbml_model('iJN746.xml')
 print(model.summary()) #products excreted
+print('-------------')
 opt_model= model.optimize()
 obj_model= opt_model.objective_value
 print(obj_model)
+print('-------------')
 
 model_1 = read_sbml_model('iJN746.xml')
-simul= get_simulator(model_1)
+envcond = {'EX_glc__D_e': (-6.3, 100000.0), 'EX_o2_e':(-15.34, 1000)}
+simul = get_simulator(model_1, envcond=envcond)
 result= simul.simulate(method='FBA')
 print(result) #specific growth rate
-
-'''
-envcond = {'EX_glc__D_e': (-6.3, 100000.0), 'EX_o2_e':(-15.34, 1000)}
-
-simul = get_simulator(model,envcond=envcond)
-'''
+print('----------------')
 
 #c)
 print('Reações essenciais:', '\n', simul.essential_reactions)
 print('------------')
 print('Genes essenciais:', '\n', simul.essential_genes)
 
-#d)
