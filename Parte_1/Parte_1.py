cce6e31ef89451f3401e7277dc11bf8d4aa4a3da
minimum16 <74023952+minimum16@users.noreply.github.com>
2020-12-27 16:12:30 +0000

    Changes

--- Parte_1/Parte_1.py
+++ Parte_1/Parte_1.py
@@ -4,30 +4,25 @@ from mewpy.simulation import get_simulator
 
 
 model = read_sbml_model('iML1515.xml')
-#print(model.summary())
+print(model.summary())
+print('----------')
 
 envcond = {'EX_o2_e': (-20.0, 100000.0), 'EX_glc__D_e': (-15.0,100000.0)}
 simul= get_simulator(model, envcond=envcond)
 result= simul.simulate(method='FBA')
 
-'''
-model_bounds = {r.id:(r.lower_bound, r.upper_bound) for r in model.reactions}
-for i in model_bounds:
-    print(i, model_bounds[i])
-'''
-
 #formate: R_EX_for_e
 
 #1)
-#print(result.fluxes['EX_for_e']) #nao produz formato em condicoes ambientais
-#print('----------')
+print(result.fluxes['EX_for_e']) #nao produz formato em condicoes ambientais
+print('----------')
 
 #2)
-#print(simul.FVA(reactions=['EX_for_e'], format='df')) #minimo= 0.0; maximo= 28.29
+print(simul.FVA(reactions=['EX_for_e'], format='df')) #minimo= 0.0; maximo= 28.29
 
 #3)
 #a)
-
+'''
 PRODUCT_ID = 'EX_for_e'
 BIOMASS_ID = 'BIOMASS_Ec_iML1515_core_75p37M'
 
@@ -42,12 +37,13 @@ from mewpy.optimization import EA
 mutante = EA(problem, max_generations=50, visualizer=True)
 mutante_run= mutante.run()
 print(mutante_run)
+print('----------')
 
 from mewpy.utils.constants import ModelConstants
 ModelConstants.RESET_SOLVER = True
+'''
 
 #b)
-'''
 PRODUCT_ID = 'EX_for_e'
 BIOMASS_ID = 'BIOMASS_Ec_iML1515_core_75p37M'
 
@@ -59,10 +55,10 @@ from mewpy.problems import GKOProblem
 problem = GKOProblem(model, fevaluation=[evaluator_1, evaluator_2], envcond=envcond, candidate_max_size=5)
 
 from mewpy.optimization import EA
-mutante2 = EA(problem, max_generations=20, visualizer=True)
+mutante2 = EA(problem, max_generations=50, visualizer=True)
 mutante_run2= mutante2.run()
 print(mutante_run2)
+print('----------')
 
 from mewpy.utils.constants import ModelConstants
 ModelConstants.RESET_SOLVER = True
-'''
